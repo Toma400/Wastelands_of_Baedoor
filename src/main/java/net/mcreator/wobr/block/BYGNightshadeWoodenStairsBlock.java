81d6cf898816efab2fcd0bf7a68e9a856f86b042
Toma400 <Balmora@DESKTOP-RP03LP2>
2021-11-11 11:15:29 +0100

    6 next BYG woods added (compat)

---
+++ src/main/java/net/mcreator/wobr/block/BYGNightshadeWoodenStairsBlock.java
@@ -0,0 +1,73 @@
+
+package net.mcreator.wobr.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.wobr.itemgroup.WoBIntegrationsTabItemGroup;
+import net.mcreator.wobr.WobrModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@WobrModElements.ModElement.Tag
+public class BYGNightshadeWoodenStairsBlock extends WobrModElements.ModElement {
+	@ObjectHolder("wobr:byg_nightshade_wooden_stairs")
+	public static final Block block = null;
+	public BYGNightshadeWoodenStairsBlock(WobrModElements instance) {
+		super(instance, 1897);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(WoBIntegrationsTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends StairsBlock {
+		public CustomBlock() {
+			super(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2f, 3f)).getDefaultState(),
+					Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 3f).lightValue(0).harvestLevel(0)
+							.harvestTool(ToolType.AXE));
+			setRegistryName("byg_nightshade_wooden_stairs");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("\u00A76 Oh The Biomes You'll Go Integration"));
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 5;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
