26ad96368d1eee2a20034aa7e3d763864974c19b
Toma400 <thomasso_calgori@o2.pl>
2022-03-16 23:26:17 +0100

    Patchouli, fixes, stuff

--- src/main/java/net/mcreator/wobr/procedures/CirtainResistanceProcedure.java
+++ src/main/java/net/mcreator/wobr/procedures/CirtainResistanceProcedure.java
@@ -1,24 +1,16 @@
 package net.mcreator.wobr.procedures;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.entity.item.ItemTossEvent;
-import net.minecraftforge.common.MinecraftForge;
-
-import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
-import net.minecraft.entity.player.PlayerEntity;
 
 import net.mcreator.wobr.WobrModElements;
 import net.mcreator.wobr.WobrMod;
 
 import java.util.Map;
-import java.util.HashMap;
 
 @WobrModElements.ModElement.Tag
 public class CirtainResistanceProcedure extends WobrModElements.ModElement {
 	public CirtainResistanceProcedure(WobrModElements instance) {
 		super(instance, 1756);
-		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -29,24 +21,5 @@ public class CirtainResistanceProcedure extends WobrModElements.ModElement {
 		}
 		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
 		(itemstack).getOrCreateTag().putBoolean("Invulnerable", (true));
-	}
-
-	@SubscribeEvent
-	public void onGemDropped(ItemTossEvent event) {
-		PlayerEntity entity = event.getPlayer();
-		double i = entity.getPosX();
-		double j = entity.getPosY();
-		double k = entity.getPosZ();
-		World world = entity.world;
-		ItemStack itemstack = event.getEntityItem().getItem();
-		Map<String, Object> dependencies = new HashMap<>();
-		dependencies.put("x", i);
-		dependencies.put("y", j);
-		dependencies.put("z", k);
-		dependencies.put("world", world);
-		dependencies.put("entity", entity);
-		dependencies.put("itemstack", itemstack);
-		dependencies.put("event", event);
-		this.executeProcedure(dependencies);
 	}
 }
