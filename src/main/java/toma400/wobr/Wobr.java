b1724b41a2d868fc8f4820b2fd7738392b3ccab7
Toma400 <thomasso_calgori@o2.pl>
2023-03-02 13:42:04 +0100

    Initial buildup (Fabric 1.19, CA version)

---
+++ src/main/java/toma400/wobr/Wobr.java
@@ -0,0 +1,14 @@
+package toma400.wobr;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Wobr implements ModInitializer {
+	public static final String MOD_ID = "wobr";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
